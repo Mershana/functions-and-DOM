307d74cefd704bae50c5b953da0191018fb67b4b
Mershana <kriomaas@gmail.com>
2022-11-22 15:36:51 +0300

    09. Функция для создания таблицы

---
+++ 09. Функция для создания таблицы/js/app.js
@@ -0,0 +1,24 @@
+'use strict'
+
+// Функция для создания таблицы из массива
+
+// №1⊗jsPmFDTCFA
+// Реализуйте описанную функцию. Проверьте ее работу.
+/* let div = document.querySelector('#elem');
+
+let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+function createTableByArr(table, arr){
+	for (let i = 0; i < arr.length; i++){
+		let tr = document.createElement('tr');
+
+		for (let j = 0; j < arr[i].length; j++){
+			let td = document.createElement('td');
+			td.innerHTML = arr[i][j];
+			tr.appendChild(td);
+		}
+		table.appendChild(tr)
+	}
+}
+
+createTableByArr(div, arr); */
